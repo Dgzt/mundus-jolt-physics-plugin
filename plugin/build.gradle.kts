36f48de43a2d789eb05edd37abbb2402a8e75cb6
Zsuró Tibor <zsurotibor@gmail.com>
2025-07-20 23:02:25 +0200

    Rename some stuffs

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -41,13 +41,13 @@ java {
 }
 
 tasks.withType<Jar> {
-    archiveFileName.set("your-plugin.jar")
+    archiveFileName.set("jolt-physics-plugin.jar")
 
     // Otherwise you'll get a "No main manifest attribute" error
     manifest {
-        attributes["Plugin-Class"]= "com.github.your_name.your_plugin.YourPlugin"
-        attributes["Plugin-Id"] = "your-plugin"
-        attributes["Plugin-Provider"] = "Your Name"
+        attributes["Plugin-Class"]= "com.github.dgzt.mundus.plugin.joltphysics.plugin.JoltPhysicsPlugin"
+        attributes["Plugin-Id"] = "jolt-physics-plugin"
+        attributes["Plugin-Provider"] = "Tibor Zsuro (Dgzt)"
         attributes["Plugin-Version"] = "0.0.1"
     }
 }
--- plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/joltphysics/plugin/JoltPhysicsPlugin.kt
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/joltphysics/plugin/JoltPhysicsPlugin.kt
@@ -1,23 +1,23 @@
-package com.github.your_name.your_plugin
+package com.github.dgzt.mundus.plugin.joltphysics.plugin
 
 import com.mbrlabs.mundus.pluginapi.MenuExtension
 import com.mbrlabs.mundus.pluginapi.ui.RootWidget
 import org.pf4j.Extension
 import org.pf4j.Plugin
 
-class YourPlugin : Plugin() {
+class JoltPhysicsPlugin : Plugin() {
 
     @Extension
-    class YourMenuExtension : MenuExtension {
+    class JoltPhysicsExtension : MenuExtension {
 
         companion object {
             const val PAD = 5f
         }
 
-        override fun getMenuName(): String = "Your plugin"
+        override fun getMenuName(): String = "Jolt Physics"
 
         override fun setupDialogRootWidget(root: RootWidget) {
-            root.addLabel("Your plugin").setPad(PAD, PAD, PAD, PAD)
+            root.addLabel("Jolt Physics").setPad(PAD, PAD, PAD, PAD)
         }
 
     }

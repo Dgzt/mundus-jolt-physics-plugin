e737e81caf762fa2239fb8195e46d37812fcbfff
Zsuró Tibor <zsurotibor@gmail.com>
2025-08-06 11:20:26 +0200

    Creates terrain physics component

--- plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/joltphysics/plugin/creator/ComponentCreator.kt
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/joltphysics/plugin/creator/ComponentCreator.kt
@@ -1,11 +1,27 @@
 package com.github.dgzt.mundus.plugin.joltphysics.plugin.creator
 
+import com.github.dgzt.mundus.plugin.joltphysics.plugin.util.GameObjectUtils
+import com.github.dgzt.mundus.plugin.joltphysics.runtime.JoltPhysicsPlugin
 import com.github.dgzt.mundus.plugin.joltphysics.runtime.component.JoltPhysicsComponent
 import com.mbrlabs.mundus.commons.scene3d.GameObject
 
 object ComponentCreator {
 
     fun create(gameObject: GameObject) : JoltPhysicsComponent {
-        return JoltPhysicsComponent(gameObject, null) // TODO
+        val componentManager = JoltPhysicsPlugin.getComponentManager()
+        val physicsComponent: JoltPhysicsComponent
+
+        if (GameObjectUtils.isTerrainManagerGameObject(gameObject)) {
+            throw RuntimeException("Implement later")
+        } else if (GameObjectUtils.isTerrainGameObject(gameObject)) {
+            physicsComponent = componentManager.createTerrainPhysicsComponent(gameObject)
+        } else if (GameObjectUtils.isModelGameObject(gameObject)) {
+            throw RuntimeException("Implement later")
+        } else {
+            throw UnsupportedOperationException("Not supported game object type!")
+
+        }
+
+        return physicsComponent
     }
 }
---
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/joltphysics/plugin/util/GameObjectUtils.kt
@@ -0,0 +1,22 @@
+package com.github.dgzt.mundus.plugin.joltphysics.plugin.util
+
+import com.mbrlabs.mundus.commons.scene3d.GameObject
+import com.mbrlabs.mundus.commons.scene3d.components.Component
+import com.mbrlabs.mundus.commons.scene3d.components.ModelComponent
+import com.mbrlabs.mundus.commons.scene3d.components.TerrainComponent
+import com.mbrlabs.mundus.commons.scene3d.components.TerrainManagerComponent
+
+object GameObjectUtils {
+
+    fun isTerrainManagerGameObject(gameObject: GameObject): Boolean {
+        return gameObject.findComponentByType<TerrainManagerComponent>(Component.Type.TERRAIN_MANAGER) != null
+    }
+
+    fun isTerrainGameObject(gameObject: GameObject): Boolean {
+        return gameObject.findComponentByType<TerrainComponent>(Component.Type.TERRAIN) != null
+    }
+
+    fun isModelGameObject(gameObject: GameObject): Boolean {
+        return gameObject.findComponentByType<ModelComponent>(Component.Type.MODEL) != null
+    }
+}
